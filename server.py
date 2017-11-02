b1a8e1aa518726541b83ec14ff8752bc76f38e55
Laura Marietta Corredor Saenz <lcorreds@usersad.everis.int>
2017-11-02 13:47:58 -0500

    Estructura inicial.

--- server.py
+++ server.py
@@ -1,11 +1,24 @@
 #server.py
 #Author: DNR
 
-from bottle import route, run
+import sys
+
+sys.path.insert(0, 'src/controllers')
+
+from bottle import route, run, template
+from action_controller import ActionController as ac
+
+@route("/models/<action>")
+def model_action(action):
+    action_controller = ac(action)
+    action_controller.greeting()
+    return template('template_action', action = action)
+
+@route("/models")
 
 @route("/")
 def index():
-    return "<p>Hello World!"
+    return "<p>Hello World!</p>"
 
 
 run(host='localhost', port=1993, debug=True)
---
+++ src/controllers/action_controller.py
@@ -0,0 +1,8 @@
+
+class ActionController:
+
+    def __init__(self, action):
+        self.action = action
+
+    def greeting(self):
+        pass    
