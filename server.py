1b2ee17b885688d593bdc8cd5588746eb73a63fe
Daniel Novoa <dnovoare@everis.com>
2017-11-02 21:44:05 -0500

    Se entregan para merge con datos del repositorio remoto.

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 #server.py
 #Author: DNR
 
+import sys
+sys.path.insert(0, 'src/controller')
+
+from indexController import IndexController
 from bottle import route, run
 
 @route("/")
 def index():
-    return "<p>Hello World!"
+    controller = IndexController()
+    return controller.greeting()
 
-
-run(host='localhost', port=1993, debug=True)
+if __name__ == "__main__":
+    run(host='localhost', port=1993, debug=True, reloader=True)
+else:
+    run(host='localhost', port=1993, debug=True)
---
+++ src/controller/indexController.py
@@ -0,0 +1,12 @@
+#index.py
+#Author: DNR
+
+class IndexController:
+
+    def __init__(self):
+        print("Entra a constructor: index")
+
+    def greeting(self):
+        print("Entra a greeting")
+        return "<i>Hello World!</i>";
+
