cd19420a2350270102bd0cbcb3bbe91db7874b69
Daniel Novoa <dnovoare@everis.com>
2017-11-04 14:38:48 -0500

    Se complementa service de elementos, agregando metodos para obtener los tipos de elementos y los tipos de datos.

--- src/services/elements_service.py
+++ src/services/elements_service.py
@@ -21,7 +21,35 @@ class ElementsService(object):
         """
         if filters is None:
             filters = {}
+        else:
+            pass
 
         __query = "SELECT * FROM ELEMENTS"
+        response = self.__db.execute(__query, filters, True).fetchall()
+        return response
+
+    def get_element_types(self, filters=None):
+        """
+        get element_types in data base with filters
+        """
+        if filters is None:
+            filters = {}
+        else:
+            pass
+
+        __query = "SELECT * FROM ELEMENT_TYPES"
+        response = self.__db.execute(__query, filters, True).fetchall()
+        return response
+
+    def get_data_types(self, filters=None):
+        """
+        get data_types in data base with filters
+        """
+        if filters is None:
+            filters = {}
+        else:
+            pass
+
+        __query = "SELECT * FROM DATA_TYPES"
         response = self.__db.execute(__query, filters, True).fetchall()
         return response
