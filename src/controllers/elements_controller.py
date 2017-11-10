3d84597558839c7d21d219618aa0fa394f25335c
Daniel Novoa <dnovoare@everis.com>
2017-11-10 06:24:50 -0500

    Metodos para manejo de elementos, tipos de elementos y tipos de datos.

--- src/controllers/elements_controller.py
+++ src/controllers/elements_controller.py
@@ -3,13 +3,9 @@ elements_controller.py
 This file will handle every possible action for the elements
 """
 
-from bottle import request
-import sys
-sys.path.insert(0, "src/services")
-sys.path.insert(1, "src/objects")
-from elements_service import ElementsService as es
-from element import Element, ElementType, DataType
-from object_extend import ObjectExt
+from src.services.elements_service import ElementsService as es
+from src.objects.element import Element, ElementType, DataType
+from src.objects.object_extend import ObjectExt
 
 
 class ElementsController(ObjectExt):
@@ -18,16 +14,27 @@ class ElementsController(ObjectExt):
     """
 
     def __init__(self, action):
+        super(ElementsController, self).__init__()
         self.__es = es()
         self.elements = []
         self.element = Element()
         self.element_types = []
         self.data_types = []
+        self.element_type = ElementType()
+        self.data_type = DataType()
         self.data = {}
         self.action = action
         self.response = None
 
-    def evaluate(self, id_element=None, req=None):
+    def set_data(self):
+        """
+        set_data docstring
+        """
+        self.data = {"elements": self.elements, "element_types": self.element_types,
+                     "data_types": self.data_types, "element": self.element, "data_type": self.data_type,
+                     "element_type":self.element_type}
+
+    def evaluate_elements(self, id_element=None, req=None):
         """
         Metodo que evalua la accion a realizar y ejecuta los metodos pertinentes
         """
@@ -43,22 +50,60 @@ class ElementsController(ObjectExt):
         elif action == 'delete':
             self.delete_element(id_element)
         else:
-            pass
+            self.get_elements()
 
-        self.get_data()
-
-
-    def get_data(self):
-        """
-        get_data docstring
-        """
-        self.get_data_types()
         self.get_element_types()
-        self.get_elements()
+        self.set_data()
 
-        self.data = {"elements":self.elements, "element_types":self.element_types,
-                     "data_types":self.data_types, "element":self.element}
+    def evaluate_element_types(self, id_element_type=None, req=None):
+        """
+        evaluate_element_types
+        """
+        action = self.action
 
+        print action, id_element_type
+
+        if action == "save":
+            self.save_element_type(id_element_type, req)
+        elif action == 'edit':
+            print "action:", id_element_type
+            self.get_element_types(id_element_type)
+        elif action == 'delete':
+            self.delete_element_type(id_element_type)
+
+        self.get_data_types()
+        self.set_data()
+
+    def evaluate_data_types(self, id_data_type=None, req=None):
+        """
+        evaluate_data_types
+        """
+        action = self.action
+
+        print action, id_data_type
+
+        if action == "save":
+            self.save_data_type(id_data_type, req)
+        elif action == 'edit':
+            print "action:", id_data_type
+            self.get_data_types(id_data_type)
+        elif action == 'delete':
+            self.delete_data_type(id_data_type)
+
+        self.set_data()
+
+    def get_element_from_row(self, row):
+        """
+        extract data to a Element object from a given row
+        """
+        c_element = Element()
+        c_element.set_id(row[0])
+        c_element.set_name(row[1])
+        c_element.set_parent_id(row[2])
+        c_element.set_type_id(row[3])
+        c_element.set_active(row[4] == 1)
+        c_element.set_container(row[5] == 1)
+        return c_element
 
     def get_elements(self, id_element=None):
         """
@@ -79,66 +124,6 @@ class ElementsController(ObjectExt):
                 elements.append(c_element)
 
             self.elements = elements
-
-    def get_element_from_row(self, row):
-        """
-        extract data to a Element object from a given row
-        """
-        c_element = Element()
-        c_element.set_id(row[0])
-        c_element.set_name(row[1])
-        c_element.set_parent_id(row[2])
-        c_element.set_type_id(row[3])
-        c_element.set_active(row[4] == 1)
-        c_element.set_container(row[5] == 1)
-        return c_element
-
-    def get_element_types(self, id_type=None):
-        """
-        get element_types in db
-        if id_element > 0 then it will filter by id
-        """
-        e_filter = {}
-        if id_type is not None:
-            e_filter["ELEMENT_TYPE_ID"] = id_type
-
-        __element_types = self.__es.get_element_types(e_filter)
-
-        element_types = []
-
-        for row in __element_types:
-            c_element = ElementType()
-            c_element.set_id(row[0])
-            c_element.set_name(row[1])
-            c_element.set_tag(row[2])
-            c_element.set_data_type_id(row[3])
-            c_element.set_parent(row[4] == 1)
-            element_types.append(c_element)
-
-        self.element_types = element_types
-
-    def get_data_types(self, id_type=0):
-        """
-        get data_types in db
-        if id_element > 0 then it will filter by id
-        """
-        e_filter = {}
-        if id_type > 0:
-            e_filter["DATA_TYPE_ID"] = id_type
-
-        __data_types = self.__es.get_data_types(e_filter)
-
-        data_types = []
-
-        for row in __data_types:
-            c_element = DataType()
-            c_element.set_id(row[0])
-            c_element.set_name(row[1])
-            c_element.set_table(row[2])
-            data_types.append(c_element)
-
-        print data_types
-        self.data_types = data_types
 
     def save_element(self, id_element, req):
         """
@@ -174,4 +159,130 @@ class ElementsController(ObjectExt):
         __id = int(self.decrypt(id_element))
 
         __response = self.__es.delete_element(__id)
+        self.response = __response
+
+    def get_element_type_from_row(self, row):
+        """
+        get_element_type_from_row
+        """
+        c_element = ElementType()
+        c_element.set_id(row[0])
+        c_element.set_name(row[1])
+        c_element.set_tag(row[2])
+        c_element.set_data_type_id(row[3])
+        c_element.set_parent(row[4] == 1)
+        return c_element
+
+    def get_element_types(self, id_type=None):
+        """
+        get element_types in db
+        if id_element > 0 then it will filter by id
+        """
+        e_filter = {}
+        if id_type is not None:
+            e_filter["ELEMENT_TYPE_ID"] = id_type
+            __element_type = self.__es.get_element_types(e_filter)
+            self.element_type = self.get_element_type_from_row(
+                __element_type[0])
+        else:
+            __element_types = self.__es.get_element_types(e_filter)
+
+            element_types = []
+
+            for row in __element_types:
+                __element_type = self.get_element_type_from_row(row)
+                element_types.append(__element_type)
+
+            self.element_types = element_types
+
+    def save_element_type(self, id_type, req):
+        """
+        save_element_type
+        """
+        __id = int(self.decrypt(id_type))
+        __name = req.forms.get("name")
+        __tag = req.forms.get("tag")
+        __data_type_id = req.forms.get("data_type_id")
+        __is_parent = req.forms.get("is_parent") is not None
+
+        if __is_parent:
+            __is_parent = 1
+        else:
+            __is_parent = 0
+
+        __element = {}
+
+        __element["ELEMENT_ID"] = __id
+        __element["ELEMENT_NAME"] = __name
+        __element["ELEMENT_TAG"] = __tag
+        __element["DATA_TYPE_ID"] = __data_type_id
+        __element["IS_PARENT"] = __is_parent
+
+        __response = self.__es.save_element_type(__element)
+        self.response = __response
+
+    def delete_element_type(self, id_type):
+        """
+        delete_element_type
+        """
+        __id = int(self.decrypt(id_type))
+
+        __response = self.__es.delete_element_type(__id)
+        self.response = __response
+
+    def get_data_type_from_row(self, row):
+        """
+        get_data_type_from_row
+        """
+        c_data_type = DataType()
+        c_data_type.set_id(row[0])
+        c_data_type.set_name(row[1])
+        c_data_type.set_table(row[2])
+        return c_data_type
+
+    def get_data_types(self, id_type=None):
+        """
+        get data_types in db
+        if id_element > 0 then it will filter by id
+        """
+        e_filter = {}
+        if id_type is not None:
+            e_filter["DATA_TYPE_ID"] = self.decrypt(id_type)
+            __data_type = self.__es.get_data_types(e_filter)
+            self.data_type = self.get_data_type_from_row(__data_type[0])
+        else:
+            __data_types = self.__es.get_data_types(e_filter)
+            data_types = []
+
+            for row in __data_types:
+                __data_type = self.get_data_type_from_row(row)
+                data_types.append(__data_type)
+
+            self.data_types = data_types
+
+    def save_data_type(self, id_data_type, req):
+        """
+        save_element
+        """
+        __id = int(self.decrypt(id_data_type))
+        __name = req.forms.get("name")
+        __table = req.forms.get("table")
+
+        __data_type = {}
+
+        __data_type["DATA_TYPE_ID"] = __id
+        __data_type["DATA_TYPE_NAME"] = __name
+        __data_type["TABLE"] = __table
+
+        __response = self.__es.save_data_type(__data_type)
+        self.response = __response
+
+
+    def delete_data_type(self, id_data_type):
+        """
+        delete_data_type
+        """
+        __id = int(self.decrypt(id_data_type))
+
+        __response = self.__es.delete_data_type(__id)
         self.response = __response
--- src/services/elements_service.py
+++ src/services/elements_service.py
@@ -33,31 +33,7 @@ class ElementsService(object):
         response = self.__db.execute(__query, filters, True).fetchall()
         return response
 
-    def get_element_types(self, filters=None):
-        """
-        get element_types in data base with filters
-        """
-        if filters is None:
-            filters = {}
-        else:
-            pass
 
-        __query = "SELECT * FROM ELEMENT_TYPES"
-        response = self.__db.execute(__query, filters, True).fetchall()
-        return response
-
-    def get_data_types(self, filters=None):
-        """
-        get data_types in data base with filters
-        """
-        if filters is None:
-            filters = {}
-        else:
-            pass
-
-        __query = "SELECT * FROM DATA_TYPES"
-        response = self.__db.execute(__query, filters, True).fetchall()
-        return response
 
     def save_element(self, element):
         """
@@ -110,6 +86,149 @@ class ElementsService(object):
 
         try:
             self.__db.execute(__delete_query, dict(ELEMENT_ID=id_element), True)
+            response = dict(error=0, text="success")
+        except Exception:
+            response = dict(error=0002, text="There was an error deleting")
+
+        return response
+
+    def get_element_types(self, filters=None):
+        """
+        get element_types in data base with filters
+        """
+        __query = "SELECT * FROM ELEMENT_TYPES"
+
+        if not filters:
+            filters = {}
+        else:
+            __query += " WHERE "
+            for key in filters:
+                __query += key + " = :" + key
+
+        response = self.__db.execute(__query, filters, True).fetchall()
+        return response
+
+    def save_element_type(self, element_type):
+        """
+        save_element_type
+        """
+        __update_query = """UPDATE ELEMENT_TYPES
+                            SET ELEMENT_TYPE_NAME = :ELEMENT_TYPE_NAME,
+                                ELEMENT_TAG = :ELEMENT_TAG,
+                                DATA_TYPE_ID = :DATA_TYPE_ID,
+                                IS_PARENT = :IS_PARENT
+                            WHERE ELEMENT_TYPE_ID = :ELEMENT_TYPE_ID
+        """
+
+        __insert_query = """INSERT INTO
+                            ELEMENT_TYPES(ELEMENT_TYPE_NAME, ELEMENT_TAG, DATA_TYPE_ID, IS_PARENT)
+                            VALUES (:ELEMENT_TYPE_NAME, :ELEMENT_TAG, :DATA_TYPE_ID, :IS_PARENT)
+        """
+
+        response = None
+
+        try:
+            if element_type["ELEMENT_TYPE_ID"] > 0:
+                print "Update", __update_query
+                self.__db.execute(__update_query, element_type, True)
+            else:
+                print "Insert"
+                del element_type["ELEMENT_TYPE_ID"]
+                self.__db.execute(__insert_query, element_type, True)
+            response = dict(error=0, text="success")
+        except Exception as e:
+            print e
+            response = dict(error=0001, text="There was an error saving")
+
+        return response
+
+    def delete_element_type(self, id_element_type):
+        """
+        delete an element with a given id if id_element_type is None then I will return an error
+        """
+        if not id_element_type:
+            return dict(error=0002, text="There was an error deleting")
+
+        __delete_query = """DELETE FROM ELEMENT_TYPES
+                            WHERE ELEMENT_TYPE_ID = :ELEMENT_TYPE_ID
+                         """
+
+        response = {}
+
+        try:
+            self.__db.execute(__delete_query, dict(
+                ELEMENT_TYPE_ID=id_element_type), True)
+            response = dict(error=0, text="success")
+        except Exception:
+            response = dict(error=0002, text="There was an error deleting")
+
+        return response
+
+
+    def get_data_types(self, filters=None):
+        """
+        get data_types in data base with filters
+        """
+        __query = "SELECT * FROM DATA_TYPES"
+
+        if not filters:
+            filters = {}
+        else:
+            __query += " WHERE "
+            for key in filters:
+                __query += key + " = :" + key
+
+        response = self.__db.execute(__query, filters, True).fetchall()
+        return response
+
+    def save_data_type(self, data_type):
+        """
+        save_data_type
+        """
+        __update_query = """UPDATE DATA_TYPES
+                            SET DATA_TYPE_NAME = :DATA_TYPE_NAME,
+                                TABLE = :TABLE
+                            WHERE DATA_TYPE_ID = :DATA_TYPE_ID
+        """
+
+        __insert_query = """INSERT INTO
+                            DATA_TYPES(DATA_TYPE_NAME, TABLE) 
+                            VALUES (:DATA_TYPE_NAME, :TABLE)
+        """
+
+        response = None
+
+        try:
+            if data_type["DATA_TYPE_ID"] > 0:
+                print "Update", __update_query
+                self.__db.execute(__update_query, data_type, True)
+            else:
+                print "Insert"
+                del data_type["DATA_TYPE_ID"]
+                self.__db.execute(__insert_query, data_type, True)
+            response = dict(error=0, text="success")
+        except Exception as e:
+            print e
+            response = dict(error=0001, text="There was an error saving")
+
+        return response
+
+
+    def delete_data_type(self, id_data_type):
+        """
+        delete an element with a given id if id_data_type is None then I will return an error
+        """
+        if not id_data_type:
+            return dict(error=0002, text="There was an error deleting")
+
+        __delete_query = """DELETE FROM DATA_TYPES
+                            WHERE DATA_TYPE_ID = :DATA_TYPE_ID
+                         """
+
+        response = {}
+
+        try:
+            self.__db.execute(__delete_query, dict(DATA_TYPE_ID=id_data_type), True)
             response = dict(error=0, text="success")
         except Exception:
             response = dict(error=0002, text="There was an error deleting")
