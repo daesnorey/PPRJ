cc993eb23f6bf42673702510c08b35072ee4e3d0
Daniel Novoa <dnovoare@everis.com>
2017-11-10 06:14:40 -0500

    Se agregan metodos que reciben peticiones de elementos, tipos de elementos y tipos de datos. ademas de la que permite referenciar archivos js y css.

--- server.py
+++ server.py
@@ -3,13 +3,9 @@ File server.py
 Server web appliation, will take the requests and route them to their controllers
 """
 
-from bottle import route, run, view, request
-from sys import path
-
-path.insert(0, 'src/controllers')
-from elements_controller import ElementsController as ec
-from models_controller import ModelsController as mc
-
+from bottle import route, run, view, request, static_file
+from src.controllers.elements_controller import ElementsController as ec
+from src.controllers.models_controller import ModelsController as mc
 
 @route("/elements")
 @route("/elements/<action>")
@@ -20,10 +16,9 @@ def elements(action="view", id_element=None):
     Metodo que manejara el comportamiento de los elementos
     """
     element_controller = ec(action)
-    element_controller.evaluate(id_element)
+    element_controller.evaluate_elements(id_element)
 
-    return dict({"action": action, "data_e": element_controller.data, "cols": 6})
-
+    return dict(action=action, data_e=element_controller.data, cols=6)
 
 @route("/elements/<action>/<id_element>", method='POST')
 def elements_action(action, id_element):
@@ -32,7 +27,57 @@ def elements_action(action, id_element):
     """
     print "elements_action", id_element
     element_controller = ec(action)
-    element_controller.evaluate(id_element, request)
+    element_controller.evaluate_elements(id_element, request)
+
+    return element_controller.response
+
+@route("/element_types")
+@route("/element_types/<action>")
+@route("/element_types/<action>/<id_type>")
+@view("element_types_template")
+def element_types(action="view", id_type=None):
+    """
+    Metodo que manejara el comportamiento de los elementos
+    """
+    element_controller = ec(action)
+    element_controller.evaluate_element_types(id_type)
+
+    return dict(action=action, data_e=element_controller.data, cols=6)
+
+
+@route("/element_types/<action>/<id_type>", method='POST')
+def element_types_action(action, id_type):
+    """
+    Metodo que manejara el comportamiento de los elementos
+    """
+    print "elements_action", id_type
+    element_controller = ec(action)
+    element_controller.evaluate_element_types(id_type, request)
+
+    return element_controller.response
+
+@route("/data_types")
+@route("/data_types/<action>")
+@route("/data_types/<action>/<id_type>")
+@view("data_types_template")
+def data_types(action="view", id_type=None):
+    """
+    Metodo que manejara el comportamiento de los elementos
+    """
+    element_controller = ec(action)
+    element_controller.evaluate_data_types(id_type)
+
+    return dict(action=action, data_e=element_controller.data, cols=6)
+
+
+@route("/data_types/<action>/<id_type>", method='POST')
+def data_types_action(action, id_element):
+    """
+    Metodo que manejara el comportamiento de los elementos
+    """
+    print "elements_action", id_element
+    element_controller = ec(action)
+    element_controller.evaluate_data_types(id_element, request)
 
     return element_controller.response
 
@@ -45,4 +90,21 @@ def models(action="view"):
     model_controller = mc(action)
     model_controller.evaluate()
 
-run(host='localhost', port=1993, debug=True, reloader=True)
+
+@route('/js/<file_name:path>')
+def js_loader(file_name):
+    """
+    import js files
+    """
+    print file_name
+    return static_file(file_name, root='./scripts')
+
+
+@route('/css/<file_name:path>')
+def css_loader(file_name):
+    """
+    import css files
+    """
+    return static_file(file_name, root='./styles')
+
+run(host='localhost', port=1996, debug=True, reloader=True)
