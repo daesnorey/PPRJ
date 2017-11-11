d14594d9ff6c27baf8a4d6372e6505cf35d04a7b
lawcorredor <lauram.co.sa@gmail.com>
2017-11-11 10:46:44 -0500

    Models
    Se crea models_service y se implemeta en el models_controller

--- src/controllers/models_controller.py
+++ src/controllers/models_controller.py
@@ -6,6 +6,7 @@ This file will handle the actions related with models
 import sys
 sys.path.insert(0, "src/services")
 sys.path.insert(1, "src/objects")
+from models_service import ModelsService as ms
 from model import Model
 
 class ModelsController(object):
@@ -14,6 +15,7 @@ class ModelsController(object):
     """
 
     def __init__(self, action):
+        self.__ms = ms()
         self.models = []
         self.action = action
 
@@ -25,10 +27,28 @@ class ModelsController(object):
 
         print action
 
-        if action is "view":
+        if action == "view":
             self.__get_models()
 
-    def __get_models(self):
+    def __get_models(self, id_model=0):
         """
-        Metodo que obtiene los modelos
+        Metodo que obtiene los modelos de bd
+        if id_model > 0 then it will filter by id
         """
+        m_filter = {}
+        if id_model > 0:
+            m_filter["MODEL_ID"] = id_model
+
+        __models = self.__ms.get_models(m_filter)
+
+        models = []
+
+        for row in __models:
+            model = Model()
+            model.set_id(row[0])
+            model.set_name(row[1])
+            model.set_active(row[2] == 1)
+            models.append(model)
+
+        print ("modelos", models)
+        self.models = models
