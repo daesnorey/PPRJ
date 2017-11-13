0ab2c3784ee1858f3bb6a16e5b20fcc0b2236336
Daniel Novoa <daesnorey@gmail.com>
2017-11-13 00:23:45 -0500

    method get_elements added to ComponentsController.

---
+++ src/controllers/components_controller.py
@@ -0,0 +1,16 @@
+from src.objects.object_extend import ObjectExt
+from src.objects.component import Component
+
+
+class ComponentsController(ObjectExt):
+
+    def __init__(self):
+        self.__cs = cs()
+        pass
+
+    def get_elements(self, id_component):
+        __id_component = self.decrypt(id_component)
+
+        e_filter = {Component.ID=__id_component}
+        __elements = self.__cs.get_elements(e_filter)
+        return __elements
