f51c1f84e96e96c9bead2d5484f79462eecac103
Daniel Novoa <daesnorey@gmail.com>
2017-11-13 18:13:58 -0500

    methods on components_service updated to use the new methods in Oracle class to save, select and delete data.

--- src/services/components_service.py
+++ src/services/components_service.py
@@ -4,6 +4,8 @@ Contain the possible actions on the components
 """
 
 from src.services.db.oracle import Oracle
+from src.objects.component import Component
+from src.objects.element import Element
 
 class  ComponentsService(object):
     """
@@ -13,21 +15,11 @@ class  ComponentsService(object):
     def __init__(self):
         self.__db = Oracle()
 
-    def get_components(self, filters=None):
+    def get_components(self, filters={}):
         """
         get components in DB with the filters
         """
-        __query = "SELECT * FROM COMPONENTS"
-
-        if not filters:
-            filters = {}
-        else:
-            __query += " WHERE "
-            for key in filters:
-                __query += key + " = :" + key
-
-        print __query
-
+        __query = self.__db.get_query("COMPONENTS", conditions=filters)
         response = self.__db.execute(__query, filters, True).fetchall()
         return response
 
@@ -35,33 +27,7 @@ class  ComponentsService(object):
         """
         save_component
         """
-        __update_query = """UPDATE COMPONENTS
-                            SET COMPONENT_NAME = :COMPONENT_NAME,
-                                IS_GENERIC = :IS_GENERIC,
-                                IS_ACTIVE = :IS_ACTIVE
-                            WHERE COMPONENT_ID = :COMPONENT_ID
-        """
-
-        __insert_query = """INSERT INTO
-                            COMPONENTS(COMPONENTS_NAME, IS_ACTIVE, IS_CONTAINER)
-                            VALUES (:COMPONENTS_NAME, :IS_ACTIVE, :IS_GENERIC)
-        """
-
-        response = None
-
-        try:
-            if component["COMPONENT_ID"] > 0:
-                print "Update", __update_query
-                self.__db.execute(__update_query, component, True)
-            else:
-                print "Insert"
-                del component["COMPONENT_ID"]
-                self.__db.execute(__insert_query, component, True)
-            response = dict(error=0, text="success")
-        except Exception as e:
-            print e
-            response = dict(error=0010, text="An error took event while it was saving a component")
-
+        response = self.__db.save("COMPONENTS", component, Component.ID)
         return response
 
     def get_elements(self, id_component):
