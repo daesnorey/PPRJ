95d31f9a517bcca3e0d8e7de3092f4365c9a4345
Daniel Novoa <daesnorey@gmail.com>
2017-11-13 00:18:38 -0500

    new method added get_join_select, for creating a select query with joins to other tables.

--- src/services/db/oracle.py
+++ src/services/db/oracle.py
@@ -70,6 +70,59 @@ class Oracle(object):
 
         return response
 
+    #######################################################################33
+    def get_join_select(self, fields=[], conditions={}, join_fields={}, *table):
+        """get_query.
+            @param table: table name in database
+            @param fields: dictionary which contain the fields to affect.
+            @param condition: dictionary which contain the fields and values to filter
+        """
+
+        print table
+        __inst  = self.get_join_instruction(fields, len(table), join_fields)
+        __inst += self.get_conditions(1, conditions)
+
+        query = __inst
+        print "BEFORE TABLE"
+        for n in range(len(table)):
+            str_replace = ":table" + str(n)
+            query = query.replace(str_replace, table[n])
+
+        return query
+
+    def get_join_instruction(self, fields, n_tables=1, join=[]):
+        """get_instruction
+	       This method will evaluate the action and will return the right instruction
+        """
+
+        __ini = "SELECT :fields FROM :table0"
+        if n_tables > 1:
+            for n in range(n_tables - 1):
+                to_join = join[n]
+                str_table = ":table" + str(n + 1)
+                str_join = ""
+                __ini += " INNER JOIN " + str_table
+                for x in to_join:
+                    if str_join:
+                        str_join += " AND "
+                    str_join += str_table + "." + x + "= :table0." + x
+                __ini += " ON " + str_join
+
+        __inst = ""
+
+        for field in fields:
+            if __inst:
+                __inst += ","
+
+            __inst += field
+
+        if not fields:
+            __inst = "*"
+
+        response = __ini.replace(":fields", __inst)
+        return response
+    #######################################################################33
+
     def get_query(self, table, fields=[], conditions={}, action=1):
         """get_query.
             @param table: table name in database
