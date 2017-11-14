f9b23f71b94523965eada7325b72fa2876742c63
Daniel Novoa <daesnorey@gmail.com>
2017-11-13 19:57:57 -0500

    Errors reported by lint fixed.

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+"""server.py.
+
+Server web appliation, will take the requests and route them to their
+controllers
 """
-File server.py
-Server web appliation, will take the requests and route them to their controllers
-"""
+
 
 from bottle import route, run, view, request, static_file
 from src.controllers.elements_controller import ElementsController as ec
@@ -9,23 +11,24 @@ from src.controllers.components_controller import ComponentsController as cc
 from src.controllers.models_controller import ModelsController as mc
 from src.objects.action import Action as actions
 
+
 @route("/elements")
 @route("/elements/<action>")
 @route("/elements/<action>/<id_element>")
 @view("elements_template")
 def elements(action="view", id_element=None):
-    """
-    Metodo que manejara el comportamiento de los elementos
-    """
+    """Metodo que manejara el comportamiento de los elementos."""
     element_controller = ec(action)
     element_controller.evaluate_elements(id_element)
 
     return dict(action=action, data_e=element_controller.data, cols=6)
 
+
 @route("/elements/<action>/<id_element>/childs")
 @view("elements_template")
 def element_childs(action, id_element):
-    """
+    """element_childs.
+
     element_childs method will handle every behaviour with the childrens
     of a specific element
     """
@@ -34,25 +37,23 @@ def element_childs(action, id_element):
 
     return "Hello world" + id_element
 
+
 @route("/elements/<action>/<id_element>", method='POST')
 def elements_action(action, id_element):
-    """
-    Metodo que manejara el comportamiento de los elementos
-    """
+    """Metodo que manejara el comportamiento de los elementos."""
     print "elements_action", id_element
     element_controller = ec(action)
     element_controller.evaluate_elements(id_element, request)
 
     return element_controller.response
 
+
 @route("/element_types")
 @route("/element_types/<action>")
 @route("/element_types/<action>/<id_type>")
 @view("element_types_template")
 def element_types(action="view", id_type=None):
-    """
-    Metodo que manejara el comportamiento de los elementos
-    """
+    """Metodo que manejara el comportamiento de los elementos."""
     element_controller = ec(action)
     element_controller.evaluate_element_types(id_type)
 
@@ -61,23 +62,20 @@ def element_types(action="view", id_type=None):
 
 @route("/element_types/<action>/<id_type>", method='POST')
 def element_types_action(action, id_type):
-    """
-    Metodo que manejara el comportamiento de los elementos
-    """
+    """Metodo que manejara el comportamiento de los elementos."""
     print "elements_action", id_type
     element_controller = ec(action)
     element_controller.evaluate_element_types(id_type, request)
 
     return element_controller.response
 
+
 @route("/data_types")
 @route("/data_types/<action>")
 @route("/data_types/<action>/<id_type>")
 @view("data_types_template")
 def data_types(action="view", id_type=None):
-    """
-    Metodo que manejara el comportamiento de los elementos
-    """
+    """Metodo que manejara el comportamiento de los elementos."""
     element_controller = ec(action)
     element_controller.evaluate_data_types(id_type)
 
@@ -86,18 +84,18 @@ def data_types(action="view", id_type=None):
 
 @route("/data_types/<action>/<id_type>", method='POST')
 def data_types_action(action, id_type):
-    """
-    Metodo que manejara el comportamiento de los elementos
-    """
+    """Metodo que manejara el comportamiento de los elementos."""
     print "elements_action", id_type
     element_controller = ec(action)
     element_controller.evaluate_data_types(id_type, request)
 
     return element_controller.response
 
+
 @route("/components/<id_component>/elements")
 @view("elements_template")
 def component_elements(id_component):
+    """Component elements."""
     __elements = cc().get_elements(id_component)
 
     elements_controller = ec(actions.VIEW)
@@ -113,9 +111,7 @@ def component_elements(id_component):
 @route("/models/<action>/<id_model>")
 @view("models_template")
 def models(action="view", id_model=None):
-    """
-    Metodo que manejara el comportamiento de los modelos
-    """
+    """Metodo que manejara el comportamiento de los modelos."""
     model_controller = mc(action)
     model_controller.evaluate(id_model)
 
@@ -123,28 +119,25 @@ def models(action="view", id_model=None):
 
     return dict(action=action, data_e=model_controller.data, cols=12)
 
+
 @route("/")
 @view("index_template")
 def index():
-    """
-    initial page, it contains links to go in a fast way to other pages.
-    """
+    """Initial page, it contains links to go in a fast way to other pages."""
     return dict()
+
 
 @route('/js/<file_name:path>')
 def js_loader(file_name):
-    """
-    import js files
-    """
+    """Import js files."""
     print file_name
     return static_file(file_name, root='./scripts')
 
 
 @route('/css/<file_name:path>')
 def css_loader(file_name):
-    """
-    import css files
-    """
+    """Import css files."""
     return static_file(file_name, root='./styles')
+
 
 run(host='localhost', port=1994, debug=True, reloader=True)
