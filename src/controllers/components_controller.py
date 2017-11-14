5d251092755434bf581dbe6837da665aa29c3a7d
Daniel Novoa <daesnorey@gmail.com>
2017-11-13 19:57:30 -0500

    ComponentsService imported, docstring added.

--- src/controllers/components_controller.py
+++ src/controllers/components_controller.py
@@ -1,12 +1,15 @@
+"""components_controller.py."""
+
 from src.objects.object_extend import ObjectExt
 from src.objects.component import Component
+from src.services.components_services import ComponentsServices as cs
 
 
 class ComponentsController(ObjectExt):
+    """ComponentsController."""
 
     def __init__(self):
         self.__cs = cs()
-        pass
 
     def get_elements(self, id_component):
         __id_component = self.decrypt(id_component)
