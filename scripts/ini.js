a80021c910a34db90d55a47851964f11e617b1de
Daniel Novoa <dnovoare@everis.com>
2017-11-17 04:34:36 -0500

    Check if there is a data-id attribute, if so then it creates an input hidden with name id and it's append to the form object.

--- scripts/ini.js
+++ scripts/ini.js
@@ -14,9 +14,15 @@
     function send() {
         var self = $( this );
         var link = self.data("link");
+        var id = self.data( "id" );
 
         var form = $( "<form method='POST'></form>" );
             form.attr( "action", link );
+
+        if( !!id ) {
+            var id_input = $( "<input type='hidden' value='" + id + "' id='id' name='id' />" );
+            form.append( id_input );
+        }
         
         $( "body" ).append( form );
         
