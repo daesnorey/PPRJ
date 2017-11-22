1f121a3261f38a1cf35c15397978178774bf892d
lawcorredor <lauram.co.sa@gmail.com>
2017-11-21 23:39:14 -0500

    model_components
    se agrega metodo para agregar componentes genericos al modelo

--- src/controllers/models_controller.py
+++ src/controllers/models_controller.py
@@ -5,9 +5,10 @@ This file will handle the actions related with models
 
 from src.services.models_service import ModelsService as ms
 from src.objects.model import Model
+from src.objects.component import Component
 from src.objects.object_extend import ObjectExt
 from src.objects.action import Action as actions
-
+from src.controllers.components_controller import ComponentsController as cc
 
 class ModelsController(ObjectExt):
     """
@@ -22,13 +23,15 @@ class ModelsController(ObjectExt):
         self.action = action
         self.data = {}
         self.response = None
+        self.is_embed = True
 
-    def evaluate(self, id_model, req=None):
+    def evaluate(self, id_model, req=None, embed=False):
         """
         Metodo que evalua la accion a realizar y ejecuta los metodos
         pertinentes
         """
         action = self.action
+        self.is_embed = embed
 
         print action
 
@@ -40,6 +43,8 @@ class ModelsController(ObjectExt):
             self.save_model(id_model, req)
         elif action == actions.DELETE:
             self.delete_model(id_model)
+        elif action == "add":
+            self.save_model_component(id_model, req)
         self.set_data()
 
     def __get_model_by_row(self, row):
@@ -71,16 +76,32 @@ class ModelsController(ObjectExt):
         """
         save_model
         """
-        __name = req.forms.get("name")
         __id = int(self.decrypt(id_model))
+        if self.is_embed:
+            __cc = cc(actions.SAVE)
+            __req_id = req.forms.get("id")
+            print "req_id", __req_id
+            __cc.evaluate(__req_id, req)
+            __component_id = __cc.response["id"]
 
-        __model = {}
+            __model_component = {}
+            __model_component[Model.ID] = __id
+            __model_component[Component.ID] = __component_id
+            __model_component["SORT"] = 99
 
-        __model[Model.ID] = __id
-        __model[Model.NAME] = __name
-        __model[Model.ACTIVE] = 1
+            self.response = self.__ms.save_model(__model_component,
+                                                 table="MODEL_COMPONENTS",
+                                                 id_name="MODEL_COMPONENT_ID")
+        else:
+            __name = req.forms.get("name")
 
-        self.response = self.__ms.save_model(__model)
+            __model = {}
+
+            __model[Model.ID] = __id
+            __model[Model.NAME] = __name
+            __model[Model.ACTIVE] = 1
+
+            self.response = self.__ms.save_model(__model)
 
     def delete_model(self, id_model):
         """
@@ -101,6 +122,23 @@ class ModelsController(ObjectExt):
             return __components
         else:
             return None
+
+    def save_model_component(self, id_model, req):
+        """
+        save_model_component
+        """
+        __id_model = int(self.decrypt(id_model))
+        __req_id = req.forms.get("id")
+        __id_component = int(self.decrypt(__req_id))
+        print "idComponent", __id_component
+        __model_component = {}
+        __model_component[Model.ID] = __id_model
+        __model_component[Component.ID] = __id_component
+        __model_component["SORT"] = 99
+
+        self.response = self.__ms.save_model(__model_component,
+                                             table="MODEL_COMPONENTS",
+                                             id_name="MODEL_COMPONENT_ID")
 
     def set_data(self):
         """
