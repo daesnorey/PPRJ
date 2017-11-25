6404b7d970336e0b685574793e75e0445ae0d3e4
Daniel Novoa <dnovoare@everis.com>
2017-11-25 13:43:32 -0500

    Add DbTypes class to handle NULL types

---
+++ src/services/db/db_types.py
@@ -0,0 +1,34 @@
+""""
+Db_types file contain DbTypes class
+"""
+
+class DbTypes(object):
+    """DbTypes."""
+
+    NULL = 'DBT_NULL'
+    NOT_NULL = 'DBT_NOT_NULL'
+
+    def __init__(self):
+        self.__sentences = {DbTypes.NULL: 'IS NULL',
+                            DbTypes.NOT_NULL: 'IS NOT NULL'}
+
+    def get_sentences(self, input_type=None):
+        """Method get_sentences."""
+        if not input_type:
+            return self.__sentences
+        else:
+            return self.__sentences[input_type]
+
+    @staticmethod
+    def get_sentence(input_type):
+        """Method get_sentence."""
+        __db_types = DbTypes()
+        if input_type in __db_types.get_sentences():
+            return __db_types.get_sentences(input_type)
+        else:
+            raise KeyError("Tipo no encontrado")
+
+    @staticmethod
+    def exist(input_type):
+        """Method exist."""
+        return input_type in DbTypes().get_sentences()
